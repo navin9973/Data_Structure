a1a2ccfb42dd5955dfe2783e816d6da4f91674ae
navin9973 <navinhjp2016@gmail.com>
2024-11-30 16:36:53 +0530

    add

--- RecursionPart3/assignment/q5.cpp
+++ RecursionPart3/assignment/q5.cpp
@@ -1,24 +1,34 @@
-#include<iostream>
-#include<vector>
-#include<climits>
+#include <iostream>
+#include <vector>
+#include <climits>
 using namespace std;
-void subset(int arr[],int n,vector<int>&v,int idx){
-  if(idx==n) {
-    for(int i=0;i<v.size();i++){
-      cout<<v[i];
-    }
-    cout<<endl;
-    return;
-  }
-  subset(arr,n,v,idx+1);
-  v.push_back(arr[idx]);
- subset(arr,n,v,idx+1);
+void subset(int arr[], int n, vector<vector<int>> &v, int idx, vector<int> &temp)
+{
+v.push_back(temp);
+  for (int i = idx; i < n; i++)
+  {
+    if (i != idx && arr[i] == arr[i - 1])
+      continue;
 
+    temp.push_back(arr[i]);
+    subset(arr, n, v, i + 1, temp);
+    temp.pop_back();
+  }
 }
- int main(){
-  int arr[]={1,2,2};
-  int n=sizeof(arr)/sizeof(arr[0]);
-  vector<int>v;
-  int idx=0;
-  subset(arr,n,v,idx);
- }
+int main()
+{
+  int arr[] = {1, 2, 2};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  vector<vector<int>> v;
+  int idx = 0;
+  vector<int> temp;
+  subset(arr, n, v, idx, temp);
+  for (int i = 0; i < v.size(); i++)
+  {
+    for(int j=0;j<v[i].size();j++){
+    cout<<v[i][j];
+  }
+  cout<<endl;
+  
+}
+}
---
+++ recursionPart4/2palindrom.cpp
@@ -0,0 +1,42 @@
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// int main(){
+//   string s="race";
+//   int i=0;
+//   int j=s.size()-1;
+//   bool flag =false;
+//   while(i<j){
+//     if(s[i]==s[j]){
+//       i++;
+//       j--;
+//       flag =true;
+//     }
+//     else{
+//       cout<<"not pelidrome";
+//       break;
+//     }
+//   }
+//   if(flag==true) cout<<"it is pelidrome";
+// }
+
+//by recursion
+
+#include<iostream>
+#include<string>
+using namespace std;
+bool pelidrome(string s,int i,int j){
+if(i>j){
+  return true;
+}
+if(s[i]==s[j]){
+  return pelidrome(s,i+1,j-1);
+}
+else return false;
+}
+int main(){
+  string s="racecar";
+  int i=0;
+  int j=s.size()-1;
+  cout<<pelidrome(s,i,j);
+}
