91481bd5d422c5dece90b18e57e24fce35555d87
navin9973 <navinhjp2016@gmail.com>
2025-01-07 14:32:31 +0530

    add

---
+++ oops/q1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class human{
+  public:
+  int hand;
+  string name;
+  int age;
+};
+int main(){
+
+human ravi;
+  ravi.hand=2;
+  ravi.name="susu";
+  cout<<ravi.name;
+
+}
